168b500d3c47543464fa0285033fd0dc76114784
unknown <sevasoroka8@gmail.com>
2024-10-29 21:29:55 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import './App.css';
+
+const App = () => { 
+  return (
+    <div className=''>
+      <header className='header'>
+      <img src = 'https://a.d-cd.net/hsAAAgOeteA-960.jpg'/>
+      </header>
+    <nav className='nav'>
+      <div>
+        Profile
+      </div>
+      <div>Messages</div>
+    </nav>
+    <div className='content'> 
+      Main Content
+    </div>
+    </div>);
+}
+
+
+
+
+export default App;
